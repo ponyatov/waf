5d6beb7e30243e00206f31069bcffb51b4ed19ef
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-10 19:49:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ inc/wasm.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+
+extern int main(int argc, char *argv[]);
+extern void arg(int argc, char *argv);
---
+++ src/wasm.cpp
@@ -0,0 +1,13 @@
+#include "wasm.hpp"
+
+int main(int argc, char *argv[]) {  //
+  arg(0, argv[0]);
+  for (int i = 1; i < argc; i++) {  //
+    arg(i, argv[i]);
+  }
+  return 0;
+}
+
+void arg(int argc, char *argv) {  //
+  std::clog << "argv[" << argc << "] = <" << argv << "]\n";
+}
