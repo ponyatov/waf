62f337953c13d58b0f7edfa20c95c2eeb59ea02f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-11 08:33:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/rule.mk
+++ mk/rule.mk
@@ -1,6 +1,12 @@
+bin/$(BINFILE): $(C) $(H) $(CP) $(HP) $(MK) $(CM)
+	cmake --fresh --preset linux
+	cmake --build --preset linux -j
+
 bin/%.wasm: src/%.wat
-	wat2wasm $< -o $@ && wasm-objdump -x $@
+# wat2wasm $< -o $@ && wasm-objdump -x $@
+	cmake --build --preset wasm -j
 tmp/%.wat: bin/%.wasm
-	wasm2wat $< -o $@
+# wasm2wat $< -o $@
+	cmake --build --preset wasm -j
 bin/%.js: src/%.js
 	cp $< $@ ; chmod +x $@
