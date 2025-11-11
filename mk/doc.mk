d5f464b3cbca1470a6484acab74d00db6ac24674
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-11 06:30:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/doc.mk
+++ mk/doc.mk
@@ -2,3 +2,6 @@
 doc:
 	rsync -r $(HOME)/metadoc/$(APP)/ doc/
 
+RF += doc/WebAssembly-3.0-draft.pdf
+doc/WebAssembly-3.0-draft.pdf:
+	$(CURL) $@ https://webassembly.github.io/spec/versions/core/WebAssembly-3.0-draft.pdf
---
+++ src/wasm.ksy
@@ -0,0 +1,5 @@
+meta:
+    id: wasm
+    file-extension: wasm
+    application: WebAssembly bytecode
+    endian: le
